08b14e1a3aa6a7ad691ef76f1b41e640ad80f719
Pradip-Dhanraj <32770009+Pradip-Dhanraj@users.noreply.github.com>
2020-05-13 00:41:23 +0530

    login UI

--- lib/pages/login-page.dart
+++ lib/pages/login-page.dart
@@ -1,4 +1,8 @@
+import 'package:chat/helper/app-routes.dart';
+import 'package:chat/helper/app-theme.dart';
 import 'package:flutter/material.dart';
+
+import 'dashboard-page.dart';
 
 class Loginpage extends StatefulWidget {
   @override
@@ -8,6 +12,171 @@ class Loginpage extends StatefulWidget {
 class _LoginpageState extends State<Loginpage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Theme.of(context).primaryColor,
+      body: ListView(
+        children: <Widget>[
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Row(children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.only(top: 50, left: 10),
+                    child: RotatedBox(
+                        quarterTurns: -1,
+                        child: Text(
+                          'Sign in',
+                          style: TextStyle(
+                            color: chambray,
+                            fontSize: 38,
+                            fontWeight: FontWeight.w900,
+                          ),
+                        )),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 10.0, left: 10.0),
+                    child: Container(
+                      //color: Colors.green,
+                      height: 200,
+                      width: 200,
+                      child: Column(
+                        children: <Widget>[
+                          Container(
+                            height: 60,
+                          ),
+                          Center(
+                            child: Text(
+                              'A world of possibility in an app',
+                              style: TextStyle(
+                                fontSize: 24,
+                                color: chambray,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ]),
+                Padding(
+                  padding: const EdgeInsets.only(left: 20, right: 20),
+                  child: Container(
+                    height: 60,
+                    width: MediaQuery.of(context).size.width,
+                    child: TextField(
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                      decoration: InputDecoration(
+                        border: InputBorder.none,
+                        fillColor: Colors.lightBlueAccent,
+                        labelText: 'Name',
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                          borderSide: BorderSide(color: breakerbay),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                          borderSide: BorderSide(color: breakerbay),
+                        ),
+                        labelStyle: TextStyle(
+                          color: breakerbay,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 20, left: 20, right: 20),
+                  child: Container(
+                    height: 60,
+                    width: MediaQuery.of(context).size.width,
+                    child: TextField(
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        border: InputBorder.none,
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                          borderSide: BorderSide(color: breakerbay),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                          borderSide: BorderSide(color: breakerbay),
+                        ),
+                        labelText: 'Password',
+                        labelStyle: TextStyle(
+                          color: breakerbay,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                Align(
+                  alignment: Alignment.centerRight,
+                  child: Padding(
+                    padding: const EdgeInsets.only(
+                      top: 20,
+                      right: 20,
+                    ),
+                    child: Container(
+                      alignment: Alignment.center,
+                      height: 50,
+                      width: MediaQuery.of(context).size.width * .4,
+                      decoration: BoxDecoration(
+                        boxShadow: [
+                          BoxShadow(
+                            color: breakerbay,
+                            blurRadius:
+                                10.0, // has the effect of softening the shadow
+                            spreadRadius:
+                                1.0, // has the effect of extending the shadow
+                            offset: Offset(
+                              5.0, // horizontal, move right 10
+                              5.0, // vertical, move down 10
+                            ),
+                          ),
+                        ],
+                        color: chambray,
+                        borderRadius: BorderRadius.circular(30),
+                      ),
+                      child: FlatButton(
+                        onPressed: () {
+                          routeToPage(
+                            context: context,
+                            action: NaivigationRoute.replace,
+                            page: Dashboard(),
+                          );
+                        },
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            Text(
+                              'LOGIN',
+                              style: TextStyle(
+                                color: wattle,
+                                fontSize: 14,
+                                fontWeight: FontWeight.w700,
+                              ),
+                            ),
+                            Icon(
+                              Icons.arrow_forward,
+                              color: wattle,
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
